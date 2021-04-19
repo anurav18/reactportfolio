26861db3fc0513991a162f4a3d496735c0bf2113
Anu Rav <anusharavva@gmail.com>
2021-04-19 12:54:22 -0700

    updated bio

--- src/components/IntroductionCard/index.js
+++ src/components/IntroductionCard/index.js
@@ -7,19 +7,26 @@ function Introductioncard() {
  <h2 class="intro-heading">Hi,I am Anusha,nice to meet you!</h2>
  <br></br>
  <p class="intro-paragraph">
- I am a SharePoint Developer/PowerApps who has immense interest in customizing the look and feel of the SharePoint Sites. 
-          In order to achieve this, I need to have the skill of using several web technologies like HTML, CSS, JavaScript, jQuery and many more, 
-          with the rapid increase in the demand for responsive web pages, I was always interested to pursue my career as a UI developer. I believe that 
-          I have a creative brain and feel like I should convert this aspect of mine into a Career path. 
-          I have done my Bachelors in Electronics and Communication Engineering from Kakatiya University, India and I have completed my master’s in electrical 
-          engineering from Northern Illinois University, Illinois. As an Electrical Engineering Student, I never really got a chance to get my hands dirty by 
-          trying to code something. As an Electrical Engineering Student, I never 
-          really got a chance to get my hands dirty by trying to code something. 
-        <br></br>
-        I have been trying for some platform which will help me fulfill my dream, one day I came across this Bootcamp which is conducted by University of
-        Washington which has the teaching of all latest technologies which are in-demand on the market. At the end of this course, I feel that I will be
-        confident enough to build a website from scratch which is responsive and has the latest features included in React. I am very much excited to be 
-        a part of this program and really looking forward to enhancing my skills.
+
+<b>"Hard work is key to Success"</b> - This is my daily Mantra! I am a technology enthusiast living in Snoqualmie, Washington. Right from my childhood, I always
+has an urge to build blocks eventually leading my passion towards engineering. I acquired a Bachelor's Degree in Electronics and Communication Engineering 
+from Kakatiya University,India and a Master's of Science Degree in Electrical Engineering from
+Northern Illinois University, Illinois. Along with that I have a certificate in Full Stack Web development from University of Washington.
+        
+<br></br>
+I was fortunate to do consulting in different domains such as State Projects, Oil & Gas, Insurance working as a SME contributing to the 
+best of my ability going above and beyond.I started my Career as a SharePoint Developer, worked on both on-premise and Online versions. During that time I 
+developed keen interest in using Power Platform capabilities to replace Infopath forms and SP Designer workflows.Along with custom development on SharePoint Online, I also started working on
+ Power Platform which is a powerful tool to create applications and solutions faster. I have developed different internal applications using Canvas Apps with a 
+ well polished UI, I have used Model driven Apps to automate the business process across the organization and attached a business process flow to it. Developed 
+ custom solutions using Javascript on PowerApps Portals.Configured COE for Power Platform. Used Power automate to create workflows and Power BI to create analytics reports. During my certification program at University of Washington I have 
+ developed a great expertise in HTML, CSS, JavaScript with an emphasis on Mern Stack Development. 
+<br></br>
+I am known for my ability to solve problems and troubleshoot technical errors, especially in code created by teammates. 
+With each project, my goal is to anticipate every feature a user might expect to have - even if they weren’t included in 
+the original design. My educational and professional background in technology gives me a broad perspective to effectively 
+anticipate and resolve issues and would make me a strong addition to any organization.
+       
  </p>
 
 </div>
