1e747ed67395ae1d6b825ce7418094fab7af0d13
Anu Rav <anusharavva@gmail.com>
2021-04-20 00:15:39 -0700

    Updated LinkedIn and Resume

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,13 +7,13 @@ function Footer() {
       <div class="container">
       <div class="row align-items-start">
     <div class="col">
-    <a href={process.env.PUBLIC_URL+"/images/AnushaRavva_Resume.pdf"} download="Anusha_Resume" class="contact-card"><i class="fas fa-file-pdf"></i>  Resume</a>
+    <a href={process.env.PUBLIC_URL+"/images/Resume.pdf"} download="Anusha_Resume" class="contact-card"><i class="fas fa-file-pdf"></i>  Resume</a>
     </div>
     <div class="col">
     <a href="https://github.com/anurav18/" target="_blank" class="contact-card"><i class="fab fa-github"></i>  gitHub </a>
     </div>
     <div class="col">
-    <a href="https://www.linkedin.com/in/anusha-r-068011123/" target="_blank" class="contact-card"><i class="fab fa-linkedin-in"></i>  linkedIn </a>
+    <a href="https://www.linkedin.com/in/anusharavva/" target="_blank" class="contact-card"><i class="fab fa-linkedin-in"></i>  linkedIn </a>
     </div>
     <div class="col">
    <a href=""> <i class="fas fa-envelope-open-text"></i> anusharavva@gmail.com </a>
